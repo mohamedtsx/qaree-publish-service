a7d80da65ad858e33cd889b5512d8075ba5d3710
mohamedtsx <mohamed.tsx@gmail.com>
2024-03-10 16:04:28 +0200

    feat(app): theme structure
    setup shadcn ui for colors and reusable components

---
+++ src/components/Hello.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Hello() {
+  return (
+    <div className="text-7xl font-semibold text-primary-foreground ">
+      <div>Qaree Landing Page</div>
+    </div>
+  );
+}
+
+export default Hello;
