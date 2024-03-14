f9bbbf25725d36da17de1c730298dfea76b5d0ac
mohamedtsx <mohamed.tsx@gmail.com>
2024-03-14 13:34:18 +0200

    chore: additional form ui components

--- src/components/ui/label.tsx
+++ src/components/ui/label.tsx
@@ -6,7 +6,7 @@ import * as React from "react";
 
 import { cn } from "@/lib/utils";
 
-const labelVariants = cva(
+export const labelVariants = cva(
 	"text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70",
 );
 
