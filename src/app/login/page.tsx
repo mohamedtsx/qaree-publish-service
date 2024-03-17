118c51ff4c018173a0c71080755c116af180bb11
mohamedtsx <mohamed.tsx@gmail.com>
2024-03-17 05:31:47 +0200

    feat: redirect to home dashboard if user loged in

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,16 +1,30 @@
 import AuthLoginForm from "@/components/AuthLoginForm";
+import ThemeToggle from "@/components/ThemeToggle";
+import { authOptions } from "@/lib/authOptions";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
 import type { Metadata } from "next/types";
-import React from "react";
 
 export const metadata: Metadata = {
 	title: "Sign In",
 	description: "Sign in to Qaree publish service",
 };
 
-function Login() {
+async function Login() {
+	const session = await getServerSession(authOptions);
+
+	if (session) {
+		redirect("/dashboard");
+	}
+
 	return (
-		<div className="container flex items-center h-full justify-center">
-			<AuthLoginForm />
+		<div className="container h-full flex flex-col">
+			<div className="w-full flex justify-end py-5">
+				<ThemeToggle />
+			</div>
+			<div className="flex-1 flex items-center justify-center">
+				<AuthLoginForm />
+			</div>
 		</div>
 	);
 }
