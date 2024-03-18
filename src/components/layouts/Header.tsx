bd7fd0cb0bd606da09325690e48be6b32496cdbc
mohamedtsx <mohamed.tsx@gmail.com>
2024-03-18 02:09:26 +0200

    feat: add help page

--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -3,7 +3,7 @@ import ThemeToggle from "../ThemeToggle";
 
 function Header() {
 	return (
-		<header className="flex justify-between h-14 items-center gap-4 border-b lg:h-[60px] px-24 ">
+		<header className="flex justify-between h-14 items-center gap-4 border-b lg:h-[60px] px-24 mb-12">
 			<SearchForm />
 			<ThemeToggle />
 		</header>
