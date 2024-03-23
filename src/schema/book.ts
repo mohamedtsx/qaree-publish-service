2d1e5eebb9c63070e42070b4db02645ce22443c1
mohamedtsx <mohamed.tsx@gmail.com>
2024-03-23 14:16:14 +0200

    feat: add starter book publishing form schema

--- src/schema/book.ts
+++ src/schema/book.ts
@@ -1,5 +1,6 @@
 import { z } from "zod";
 
+// todo add better validation
 const filesSchema = z.object({
 	cover: z.instanceof(File, {
 		message: "Please select a book cover image.",
@@ -19,3 +20,16 @@ const bookDetailesSchema = z.object({
 	language: z.string(),
 	publishingRights: z.boolean(),
 });
+
+type MediaType = z.infer<typeof filesSchema>;
+
+export const publishDefaultValues: Omit<PublishSchemaType, keyof MediaType> = {
+	name: "",
+	categories: [""],
+	description: "",
+	language: "",
+	publishingRights: false,
+};
+
+export const publishSchema = bookDetailesSchema.and(filesSchema);
+export type PublishSchemaType = z.infer<typeof publishSchema>;
