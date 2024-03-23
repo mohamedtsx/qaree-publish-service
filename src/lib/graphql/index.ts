6501469f9822b3ae73146ccc7b8ac83089fbaa60
mohamedtsx <mohamed.tsx@gmail.com>
2024-03-23 17:49:18 +0200

    feat: add upload files base url

--- src/lib/graphql/index.ts
+++ src/lib/graphql/index.ts
@@ -1,3 +1,9 @@
 import { env } from "@/env";
 
 export const BACKEND_URL = env.NEXT_PUBLIC_BACKEND_URL;
+
+export const UPLOAD_FULL_URL = {
+	cover: (bookId: string) => `${BACKEND_URL}/book/cover/${bookId}`,
+	file: (bookId: string) => `${BACKEND_URL}/book/file/${bookId}`,
+	sample: (bookId: string) => `${BACKEND_URL}/book/sample/${bookId}`,
+};
