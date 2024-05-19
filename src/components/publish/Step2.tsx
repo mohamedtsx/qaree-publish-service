b33dc811da147e89ec380da61814b56af2dbc7eb
mohamedtsx <mohamed.tsx@gmail.com>
2024-05-19 12:12:05 +0300

    feat: step 2 submit handler flow

--- src/components/publish/Step2.tsx
+++ src/components/publish/Step2.tsx
@@ -1,6 +1,8 @@
+import { addBookSampleAction } from "@/app/actions";
 import { zodResolver } from "@hookform/resolvers/zod";
 import React from "react";
 import { useForm } from "react-hook-form";
+import { toast } from "sonner";
 import { z } from "zod";
 
 const bookContentSchema = z.object({
@@ -13,8 +15,18 @@ const bookContentSchema = z.object({
 
 type BookContentSchema = z.infer<typeof bookContentSchema>;
 
-export const Step2 = ({ onDone }: { onDone: () => void }) => {
-	const onSubmit = (values: BookContentSchema) => {
+export const Step2 = ({
+	onDone,
+	data,
+}: {
+	onDone: () => void;
+	data: {
+		bookId: string;
+		defaultValues?: BookContentSchema;
+	};
+}) => {
+	const onSubmit = async (values: BookContentSchema) => {
+		// if form is not dirty you could call onDone directly here
 		if (!values.coverUploaded) {
 			form.setError("coverUploaded", {
 				message: "Upload a cover for the book.",
@@ -22,18 +34,31 @@ export const Step2 = ({ onDone }: { onDone: () => void }) => {
 		}
 
 		if (!values.fileUploaded) {
-			form.setError("fileUploaded", { message: "Upload the ebook file." });
+			return form.setError("fileUploaded", {
+				message: "Upload the ebook file.",
+			});
 		}
 
 		if (!values.sample.length) {
 			form.setError("sample", { message: "Select at least one sample item." });
 		}
+
+		const { sucess, message } = await addBookSampleAction({
+			bookId: data.bookId,
+			sample: values.sample,
+		});
+
+		if (!sucess) {
+			return toast.error(message);
+		}
+
+		onDone();
 	};
 
 	const form = useForm<BookContentSchema>({
 		mode: "onBlur",
 		resolver: zodResolver(bookContentSchema),
-		defaultValues: {
+		defaultValues: data.defaultValues || {
 			coverUploaded: false,
 			fileUploaded: false,
 			sample: [],
