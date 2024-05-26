3b9ccb816edd3cfda507e4465d877bbed29765c7
mohamedtsx <mohamed.tsx@gmail.com>
2024-05-26 14:32:03 +0300

    chore(deps): install million lint

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,14 @@
+import MillionLint from '@million/lint';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	images: {
-		remotePatterns: [
-			{
-				protocol: "https",
-				hostname: "res.cloudinary.com",
-				port: "",
-			},
-		],
-	},
+  images: {
+    remotePatterns: [{
+      protocol: "https",
+      hostname: "res.cloudinary.com",
+      port: ""
+    }]
+  }
 };
-
-export default nextConfig;
+export default MillionLint.next({
+  rsc: true
+})(nextConfig);
