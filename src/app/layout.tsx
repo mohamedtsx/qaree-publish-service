3f7f64b2476c1dac0382f76031e0bd6f5d06b3d1
mohamedtsx <mohamed.tsx@gmail.com>
2024-06-02 21:35:29 +0300

    fix: child hight not full screen

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,8 +19,8 @@ export default function RootLayout({
 	children: React.ReactNode;
 }>) {
 	return (
-		<html lang="en" className="no-scrollbar" suppressHydrationWarning>
-			<body className={cn("flex h-full antialiased", inter.className)}>
+		<html lang="en" className="h-full no-scrollbar" suppressHydrationWarning>
+			<body className={cn("flex  h-full antialiased", inter.className)}>
 				<Providers>
 					<div className="flex-1">{children}</div>
 				</Providers>
