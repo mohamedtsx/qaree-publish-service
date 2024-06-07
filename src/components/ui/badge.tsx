c420d0108310371b0a5a697a04a29350e7b64388
mohamedtsx <mohamed.tsx@gmail.com>
2024-06-07 12:34:22 +0300

    feat: new custome variants to badge notion_incomplete & notion_done style

--- src/components/ui/badge.tsx
+++ src/components/ui/badge.tsx
@@ -15,6 +15,10 @@ const badgeVariants = cva(
 				destructive:
 					"border-transparent bg-destructive text-destructive-foreground shadow hover:bg-destructive/80",
 				outline: "text-foreground",
+				notion_done:
+					"whitespace-nowrap overflow-hidden overflow-ellipsis before:size-2 before:rounded-full before:bg-secondary-green before:inline-flex before:shrink-0 before:mr-1.5  min-w-0 max-w-full h-7 rounded-xl text-sm pl-2 pr-4 leading-tight m-0 capitalize bg-secondary-green/40 border-secondary-green ",
+				notion_incomplete:
+					"whitespace-nowrap overflow-hidden overflow-ellipsis before:size-2 before:rounded-full before:bg-destructive before:inline-flex before:shrink-0 before:mr-1.5  min-w-0 max-w-full h-7 rounded-xl text-sm pl-2 pr-4 leading-tight m-0 capitalize bg-destructive/40 border-destructive text-destructive-foreground",
 			},
 		},
 		defaultVariants: {
