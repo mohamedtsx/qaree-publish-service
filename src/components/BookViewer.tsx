91885e72feeae8be2fb7d499a6303b32c6b5ee59
mohamedtsx <mohamed.tsx@gmail.com>
2024-06-08 15:18:03 +0300

    feat: place reading viewer in a dialog

--- src/components/BookViewer.tsx
+++ src/components/BookViewer.tsx
@@ -1,9 +1,17 @@
 import { getCurrentUser } from "@/lib/authOptions";
 import { BACKEND_BASE_URL } from "@/lib/graphql";
-import { Button } from "./ui/button";
 import { BookViewerContainer } from "./BookViewerContainer";
 import { getHtmlContentQuery } from "@/lib/graphql/queries";
 import { fetcher } from "@/lib/graphql/fetcher";
+import {
+	Dialog,
+	DialogContent,
+	DialogContent2,
+	DialogOverlay,
+	DialogPortal,
+	DialogTrigger,
+} from "./ui/dialog";
+import { ReactNode } from "react";
 
 const getHtmlContent = async (bookId: string) => {
 	const { getBookEPubContent } = await fetcher({
@@ -76,12 +84,28 @@ export async function BookViewer({ bookId, contentId }: Props) {
 	const htmlText = await getBookChapter(bookId, contentId ?? htmlContent[0].id);
 
 	return (
-		<BookViewerContainer content={htmlContent}>
-			<div
-				className=" bg-muted px-4"
-				// biome-ignore lint: using dangerouslySetInnerHTML
-				dangerouslySetInnerHTML={{ __html: htmlText }}
-			/>
-		</BookViewerContainer>
+		<Dialog>
+			<DialogTrigger>click me</DialogTrigger>
+			<DialogContent2 className="max-w-5xl">
+				<BookViewerContainer content={htmlContent}>
+					<div
+						className=" bg-muted px-4"
+						// biome-ignore lint: using dangerouslySetInnerHTML
+						dangerouslySetInnerHTML={{ __html: htmlText }}
+					/>
+				</BookViewerContainer>
+			</DialogContent2>
+		</Dialog>
 	);
 }
+
+// const CustomeDialogContent = ({ children }: { children: ReactNode }) => {
+// 	return (
+// 		<DialogPortal>
+// 			<DialogOverlay />
+// 			<DialogPrimitive.Content className="fixed left-[50%] top-[50%] z-50 grid w-full max-w-lg translate-x-[-50%] translate-y-[-50%] gap-4 border bg-background p-6 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] sm:rounded-lg">
+// 				{children}
+// 			</DialogPrimitive.Content>
+// 		</DialogPortal>
+// 	);
+// };
